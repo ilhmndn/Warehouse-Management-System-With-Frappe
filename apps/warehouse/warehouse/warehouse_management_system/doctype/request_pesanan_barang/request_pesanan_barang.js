93d42ac7a8f92226d8fe6be0777281cf55292926
ilham aminudin <ilhmndn@localhost.localdomain>
2019-07-01 13:37:53 +0700

    commit 1 juli (total harga sukses)

--- apps/warehouse/warehouse/warehouse_management_system/doctype/request_pesanan_barang/request_pesanan_barang.js
+++ apps/warehouse/warehouse/warehouse_management_system/doctype/request_pesanan_barang/request_pesanan_barang.js
@@ -1,12 +1,26 @@
 // Copyright (c) 2019, Kelompok 4 and contributors
 // For license information, please see license.txt
 
+
 frappe.ui.form.on('Request Pesanan Barang', 'tanggal_request', function(frm) {
 	if(frm.doc.tanggal_request < get_today()) {
 		frm.set_value('tanggal_request','')
 			frappe.throw('Mohon Maaf Harus Memilih Tanggal Hari Ini!!!!')
 	}
 });
+
+
+frappe.ui.form.on("Request Line Pesanan", "qty", function(frm, cdt, cdn) {
+
+	var harga = frm.doc.request_line_pesanan;
+	var total_harga = 0
+
+	for(var i in harga) {
+	total_harga = (total_harga + (parseInt(harga[i].qty) * parseInt(harga[i].harga)))
+	 }
+		frm.set_value("total_harga",total_harga) 
+	
+  });
 
 // cur_frm.set_query('kode_barang','request_line_pesanan', function(doc, cdt, cdn) {
 // 	var d = locals[cdt][cdn];
